1cd917894bac32ca1a2a4efae912ee58aafc9d13
HZ <will3359@gmail.com>
2018-11-01 16:20:36 -0700

    Updated toString method

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -12,15 +12,16 @@ public class User {
 		this.name = name;
 	}
 	
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
-	
 	public int getAge() {
 		return age;
 	}
 	public void setAge(int age) {
 		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
 	}	
+	
 }
