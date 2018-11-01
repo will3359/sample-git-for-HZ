34a78191bdef7295ce13f2fe485690a154e0e875
HZ <will3359@gmail.com>
2018-11-01 16:04:33 -0700

    Added field age

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -3,4 +3,14 @@ package org.paumard.model;
 public class User {
 
 	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
