5896308b3f973b41d8c74adcd05cc3ed7c5de4fa
HZ <will3359@gmail.com>
2018-11-02 09:27:51 -0700

    Added Constructors

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -5,6 +5,14 @@ public class User {
 	private String name;
 	private int age;
 	
+	public User() {
+		super();
+	}
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 	public String getName() {
 		return name;
 	}
